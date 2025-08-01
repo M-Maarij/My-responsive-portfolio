da24a8c995c1d633cb25fe224605a232eed62ffa
M-Maarij <maarijmoeen12345@gmail.com>
2025-08-01 17:05:43 +0500

    Added initial html/css structure

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Portfolio loaded!"); // Test if JS is linked
+// Example: Toggle a mobile menu
+document.querySelector('.menu-button').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
